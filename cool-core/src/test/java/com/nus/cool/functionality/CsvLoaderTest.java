db1139fc25f1b0e1b4ec0357f6800104e63131fe
Qingpeng Cai <qingpeng@u.nus.edu>
2022-05-02 16:08:56 +0800

    Remove dim file in the csv loader

--- cool-core/src/test/java/com/nus/cool/functionality/CsvLoaderTest.java
+++ cool-core/src/test/java/com/nus/cool/functionality/CsvLoaderTest.java
@@ -11,23 +11,22 @@ import java.util.Properties;
 
 public class CsvLoaderTest {
 
-    @Test(priority = 10, enabled = true)
+    @Test(priority = 10)
     public static void CsvFileExistTest() throws IOException {
-        System.out.println("======================== Csv data loader failure Test ========================");
+        System.out.println("======================== Csv data loader Test ========================");
         // System.out.println(System.getProperty("user.dir"));
         CsvProperties csvps= new CsvProperties();
         Properties csvp=csvps.getProperties();
         String cube = csvp.getProperty("cube_0");
         String schemaFileName =  csvp.getProperty("schemaFileName_0");
-        String dimFileName =  csvp.getProperty("dimFileName_0");
         String dataFileName = csvp.getProperty("dataFileName_0");
         String cubeRepo =  csvp.getProperty("cubeRepo_0");
         DataLoaderConfig config = new CsvDataLoaderConfig();
         CoolLoader loader = new CoolLoader(config);
-        loader.load(cube, schemaFileName, dimFileName, dataFileName, cubeRepo);
+        loader.load(cube, schemaFileName, dataFileName, cubeRepo);
         System.out.println("Csv file load correctly");
     }
-    @Test(expectedExceptions = {IOException.class}, priority = 10, enabled = true)
+    @Test(expectedExceptions = {IOException.class}, priority = 10)
     public static void CsvFileNonExistTest() throws IOException {
         System.out.println("======================== Csv data loader failure Test ========================");
         // System.out.println(System.getProperty("user.dir"));
@@ -35,11 +34,10 @@ public class CsvLoaderTest {
         Properties csvp=csvps.getProperties();
         String cube = csvp.getProperty("cube_1");
         String schemaFileName =  csvp.getProperty("schemaFileName_1");
-        String dimFileName =  csvp.getProperty("dimFileName_1");
         String dataFileName = csvp.getProperty("dataFileName_1");
         String cubeRepo =  csvp.getProperty("cubeRepo_1");
         DataLoaderConfig config = new CsvDataLoaderConfig();
         CoolLoader loader = new CoolLoader(config);
-        loader.load(cube, schemaFileName, dimFileName, dataFileName, cubeRepo);
+        loader.load(cube, schemaFileName, dataFileName, cubeRepo);
     }
 }
